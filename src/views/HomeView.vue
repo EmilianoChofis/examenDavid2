d36f535499f3aeccad5c518d27cfbe76b8e4f32f
EmilianoChofis <20213tn128@utez.edu.mx>
2024-03-04 18:45:39 -0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+import MoviesCard from "@/components/MoviesCard.vue";
+import Carousel from "@/components/Carousel.vue";
+</script>
+
+<template>
+  <main>
+    <Carousel/>
+    <MoviesCard/>
+  </main>
+</template>
